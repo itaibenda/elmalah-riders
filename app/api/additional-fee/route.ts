f3c9d199e418dea918f78fa2acd6f5a44b87bd04
Itai Ben David <itaibd@wix.com>
2025-02-13 15:26:06 +0200

    added spi

---
+++ app/api/additional-fee/route.ts
@@ -0,0 +1,43 @@
+import { AppStrategy, createClient } from '@wix/sdk';
+import { additionalFees } from '@wix/ecom/service-plugins';
+import { NextRequest, NextResponse } from 'next/server';
+import { error } from 'console';
+
+const wixClient = createClient({
+  auth: AppStrategy({
+    appId: '5e3d205a-cca8-4b0c-8ac2-03bdc4e7476b',
+    appSecret: '894c03b7-2c99-4c23-945d-f72f62e1cb1d',
+  }),
+  modules: { additionalFees },
+});
+
+wixClient.additionalFees.provideHandlers({
+  calculateAdditionalFees: async (payload) => {
+    const { request, metadata } = payload;
+
+    // Add your logic here
+    return {
+      additionalFees: [
+        {
+          code: 'gift-wrap',
+          name: 'Gift Wrapping',
+          price: '5.00',
+          taxDetails: { taxable: true },
+        },
+      ],
+      currency: metadata.currency || 'USD',
+    };
+  },
+});
+
+export async function GET(request: NextRequest) {
+  console.log('!!! here');
+
+  try {
+    return NextResponse.json(
+      await wixClient.servicePlugins.processRequest(request)
+    );
+  } catch (e) {
+    return NextResponse.json(error);
+  }
+}
