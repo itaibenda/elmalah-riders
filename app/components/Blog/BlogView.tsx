f71dd0f79645d77ac1365873bd342d0d5ea8b1bf
Itai Ben David <itaibd@wix.com>
2025-02-27 22:41:27 +0200

    better looking blog

--- app/components/Blog/BlogView.tsx
+++ app/components/Blog/BlogView.tsx
@@ -22,15 +22,15 @@ export function BlogView({
     // oops can't mutate the likes!
   };
   return (
-    <div className="mx-auto px-14 mt-12">
+    <div className="max-w-4xl mx-auto px-8 sm:px-14 pt-4 sm:pt-16">
       {post ? (
         <div
           className="full-w overflow-hidden"
           data-testid={testIds.PRODUCT_DETAILS.CONTAINER}
         >
-          <h1>{post.title}</h1>
+          <h1 className="text-3xl leading-none">{post.title}</h1>
           <div className="">
-            <div className="box-border flex flex-col">
+            <div className="box-border flex flex-col py-6">
               <div>
                 <WixMediaImage media={post.media!.wixMedia?.image} />
               </div>
